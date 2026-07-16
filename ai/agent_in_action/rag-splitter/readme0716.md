0d886dc01de9f74e2b8e3000c554eddee1b252c8
dudu <2724231733@qq.com>
2026-07-16 21:18:50 +0800

    0716

--- ai/agent_in_action/rag-splitter/readme0716.md
+++ ai/agent_in_action/rag-splitter/readme0716.md
@@ -19,3 +19,20 @@ pageMetadata 是文档的元数据
 2. 切割文档，文件太大，要检索的是一定大小具有一定语义的chunk
 来自社区@langchain/community 主要由社区维护，我们都可以写loader
 langchain @langchain/core 官方维护的
+
+- 爬虫 crawl
+ - 从目标url开始,发送请求,拿到html字符串
+ - 解析html字符串,提取出文档的内容(以前会使用正则表达式,现在使用cheerio库)
+ - cheerio 另辟蹊径,前端思维 css 选择器 选择需要的内容
+ - cheerio.load(html)
+   $(css selector).text()
+
+## AI时代程序员价值
+ - 不再是coding ,交给AI
+ - vibe coding 问出好的问题(prompt),提供丰富准确的上下文(context),去驾驭(Harness)并且部署(FDE)Agent产品,设计长时间稳定运行的loop,用好AI,快速成为一名AI 架构师
+
+- 切割的意义
+保持语义的完整性
+- separators 语义的最基本构成符号 。! ?
+- 按chunksize 大小切割
+- 切断了,chunk的最后一句和下一个chunk第一句,他们的语义相关性是最大的,但是因为chunsize切开了,用overlap来确保语义的完整性
--- ai/agent_in_action/rag-splitter/src/index.mjs
+++ ai/agent_in_action/rag-splitter/src/index.mjs
@@ -1,16 +1,40 @@
 import "dotenv/config";
 import "cheerio";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 //从url 加载文档
 import {
+  // loader 按url 加载
   CheerioWebBaseLoader
 } from '@langchain/community/document_loaders/web/cheerio'
 // 访问网址 并提取文档内容
 // cheerio 可以传递css 选择器 来提取文档内容 缩小范围
+// 爬取指定内容 + Document 标准化
 const cheerioLoader = new CheerioWebBaseLoader(
   'https://juejin.cn/post/7660707431753678854',
   {
     selector: '.main-area p' // 文章段落
   }
 )
+// 大的document -> 小的document 更加精细的控制,和处理语义
+// 文档分割器 document splitter
+// 按照段落分? 按语义分段,但是段落可能太长或者太短
+// 分割目的是语义的精确化
+// chunk大小
 const documents = await cheerioLoader.load();
-console.log(documents)
+// console.log(documents)
+
+// 切片
+// 语义排第一
+// 按大小来切割,chunkSize就够了
+// 为了语义完整,少一点 chunkOverlap
+// 递归尝试不同的分隔符,找到最优的分隔符,使每个chunk都有语义,并且chunk大小接近chunkSize
+// 不完美的地方,直接硬切 chunkOverlap 补救
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 400,// 每个chunk大小 
+  separators: ["。", "!", "?"],
+  // 每个chunk 重叠的字符数
+  chunkOverlap: 100,
+})
+
+const splitDocuments = await textSplitter.splitDocuments(documents)
+console.log(splitDocuments)
